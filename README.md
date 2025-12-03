67fd64ec44e9aabddc8657d08ae286c7c0b0567b
RainydeVille <vaitkevicienegle@gmail.com>
2025-12-03 14:05:22 +0200

    Created React framework for future.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 This is my portfolio page. It's very simple now and a work in progress but if you want to check it out here's a live demo (I know the media is not there yet 😊 ):
 [🔗 Live Demo](https://rainydeville.github.io/portfolio/)
+
+PS: The site is under construction 🧙‍♂️ I am moving to React framework to continue working on it. So don't mind the react-version for now pls.
+
+- You can still see the old version tho.
---
+++ react-version/src/components/Header.jsx
@@ -0,0 +1,20 @@
+export default function Header() {
+  return (
+    <header>
+      <h2 id="name">Egle Vaitkeviciene</h2>
+      <nav>
+        <ul>
+          <li>
+            <a href="#aboutMe">About me</a>
+          </li>
+          <li>
+            <a href="#resume">Resume</a>
+          </li>
+          <li>
+            <a href="#projects"></a>Projects
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
